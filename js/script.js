4ffa33579d134d6fc81b0600be24b8ff8fbe0408
Jose Andres <joseandresdurand@gmail.com>
2015-10-10 19:25:38 -0500

    demo1

--- js/script.js
+++ js/script.js
@@ -1,80 +1,293 @@
-$(document).ready(function($){
-	
-	// $(".secciones").click(function () {
- //        var audio = {};
- //        audio['walk'] = new Audio();
- //        audio['walk'].src = "http://translate.google.com/translate_tts?tl=es&q=gracias+Jose"
- //        audio['walk'].addEventListener('load', function () {
- //        audio['walk'].play();
- //        });
- //    });
+$(document).ready(function(){
 
-	// Portada
-	
-	$(".portada").mouseenter(function(){
-		var audio = $("#portadasound")[0];
-		audio.play();
-	});
-	// $(".portada").mouseleave(function(){
-	// 	var audio = $("#portadasound")[0];
-	// 	audio.pause();
-	// });
+	var value = "";
+	var translate = "";
+	var leer = "";
+	var nn = $("#prueba").html();
+	var n = nn.length;
+	var i = 0;
+	var audio = $("#audioh")[0];
 
-	// Politica
 	
 
-	$(".politica").mouseenter(function(){
-		var audio = $("#politicasound")[0];
+	$("#sonido").mouseenter(function(){
+		i = 0;		
+		
+		value = $("#texto1").text();
+		leer = "http://translate.google.com/translate_tts?tl=es&q=";
+		//leerdos = "&client=t";
+
+//primera oración
+		
+		$("#audioh").attr("src", leer+value);
 		audio.play();
+
+
+		$("#audioh").on("timeupdate", function(event) {
+
+	        var currentTime = Math.ceil(this.currentTime);
+	        var duration = Math.ceil(this.duration);
+	        var pp = $("#texto1").html();
+			var p = pp.length;
+	    
+	     	$("#duracion").text("Duracion de audio" +" : "+ duration + " sg");
+	    	$("#current").text("tiempo transcurrido" + " : " + currentTime + " sg");
+	    	$("#caracteres").text("N° de Caracteres " + n);
+	    	$("#oracion").text("Caracteres en la oración " + p);
+	    	$("#parrafo").text("Sistema " + i);
+
+		
+	    
+
+//segunda oración
+		
+
+			if (currentTime < duration) {
+				$("#simulacro").text("PLAY");
+			}else{
+				$("#simulacro").text("FINALIZO");
+				
+				i = i + 1;			
+
+				value = "";
+				value = $("#texto2").text();
+				leer = "http://translate.google.com/translate_tts?tl=es&q=";
+				leerdos = "&client=t";
+
+				audio = $("#audioh")[0];
+				$("#audioh").attr("src", leer+value+leerdos);
+				audio.play();
+
+				i = i + 1;
+
+					$("#audioh").on("timeupdate", function(event) {
+
+					var currentTimeb = Math.ceil(this.currentTime);
+			        var durationb = Math.ceil(this.duration);
+			        var pp = $("#texto2").html();
+					var p = pp.length;
+
+		 			$("#duracion").text("Duracion de audio" +" : "+ durationb + " sg");
+					$("#current").text("tiempo transcurrido" + " : " + currentTimeb + " sg");
+					$("#caracteres").text("N° de Caracteres " + n);
+					$("#oracion").text("Caracteres en la oración " + p);
+					$("#parrafo").text("Sistema " + i);
+					i = i + 1;
+
+					
+						if (currentTimeb < durationb) {
+							$("#simulacro").text("PLAY");
+						}else{
+
+//tercera oración
+							$("#simulacro").text("FINALIZO");
+							
+							i = i + 1;			
+
+							value = "";
+							value = $("#texto3").text();
+							leer = "http://translate.google.com/translate_tts?tl=es&q=";
+							leerdos = "&client=t";
+
+							audio = $("#audioh")[0];
+							$("#audioh").attr("src", leer+value+leerdos);
+							audio.play();
+
+								$("#audioh").on("timeupdate", function(event) {
+								var currentTimec = Math.ceil(this.currentTime);
+						        var durationc = Math.ceil(this.duration);
+						        var pp = $("#texto3").html();
+								var p = pp.length;
+
+					 			$("#duracion").text("Duracion de audio" +" : "+ durationc + " sg");
+								$("#current").text("tiempo transcurrido" + " : " + currentTimec + " sg");
+								$("#caracteres").text("N° de Caracteres " + n);
+								$("#oracion").text("Caracteres en la oración " + p);
+								$("#parrafo").text("Sistema " + i);
+								i = i + 1;
+
+									if (currentTimec < durationc) {
+										$("#simulacro").text("PLAY");
+									}else{
+										$("#simulacro").text("FINALIZO");
+//Cuarta oracion
+										i = i + 1;			
+
+										value = "";
+										value = $("#texto4").text();
+										leer = "http://translate.google.com/translate_tts?tl=es&q=";
+										leerdos = "&client=t";
+
+										audio = $("#audioh")[0];
+										$("#audioh").attr("src", leer+value+leerdos);
+										audio.play();
+
+											$("#audioh").on("timeupdate", function(event) {
+											var currentTimed = Math.ceil(this.currentTime);
+									        var durationd = Math.ceil(this.duration);
+									        var pp = $("#texto4").html();
+											var p = pp.length;
+
+								 			$("#duracion").text("Duracion de audio" +" : "+ durationd + " sg");
+											$("#current").text("tiempo transcurrido" + " : " + currentTimed + " sg");
+											$("#caracteres").text("N° de Caracteres " + n);
+											$("#oracion").text("Caracteres en la oración " + p);
+											$("#parrafo").text("Sistema " + i);
+											i = i + 1;
+
+												if (currentTimed < durationd) {
+													$("#simulacro").text("PLAY");
+												}else{
+													$("#simulacro").text("FINALIZO");
+													i = i + 1;			
+//Quinta oracion
+													value = "";
+													value = $("#texto5").text();
+													leer = "http://translate.google.com/translate_tts?tl=es&q=";
+													leerdos = "&client=p";
+
+													audio = $("#audioh")[0];
+													$("#audioh").attr("src", leer+value+leerdos);
+													audio.play();
+
+														$("#audioh").on("timeupdate", function(event) {
+														var currentTimee = Math.ceil(this.currentTime);
+												        var duratione = Math.ceil(this.duration);
+												        var pp = $("#texto5").html();
+														var p = pp.length;
+
+											 			$("#duracion").text("Duracion de audio" +" : "+ duratione + " sg");
+														$("#current").text("tiempo transcurrido" + " : " + currentTimee + " sg");
+														$("#caracteres").text("N° de Caracteres " + n);
+														$("#oracion").text("Caracteres en la oración " + p);
+														$("#parrafo").text("Sistema " + i);
+														i = i + 1;
+
+															if (currentTimee < duratione) {
+																$("#simulacro").text("PLAY");
+															}else{
+																$("#simulacro").text("FINALIZO");
+																i = i + 1;		
+//Sexta oracion
+																value = "";
+																value = $("#texto6").text();
+																leer = "http://translate.google.com/translate_tts?tl=es&q=";
+																leerdos = "&client=p";
+
+																audio = $("#audioh")[0];
+																$("#audioh").attr("src", leer+value+leerdos);
+																audio.play();
+
+																	$("#audioh").on("timeupdate", function(event) {
+																	var currentTimee = Math.ceil(this.currentTime);
+															        var duratione = Math.ceil(this.duration);
+															        var pp = $("#texto6").html();
+																	var p = pp.length;
+
+														 			$("#duracion").text("Duracion de audio" +" : "+ duratione + " sg");
+																	$("#current").text("tiempo transcurrido" + " : " + currentTimee + " sg");
+																	$("#caracteres").text("N° de Caracteres " + n);
+																	$("#oracion").text("Caracteres en la oración " + p);
+																	$("#parrafo").text("Sistema " + i);
+																	i = i + 1;
+
+																		if (currentTimee < duratione) {
+																			$("#simulacro").text("PLAY");
+																		}else{
+																			$("#simulacro").text("FINALIZO");
+																			i = i + 1;
+																			value = "";
+																			value = $("#texto7").text();
+																			leer = "http://translate.google.com/translate_tts?tl=es&q=";
+																			leerdos = "&client=p";
+
+																			audio = $("#audioh")[0];
+																			$("#audioh").attr("src", leer+value+leerdos);
+																			audio.play();
+
+																				$("#audioh").on("timeupdate", function(event) {
+																				var currentTimef = Math.ceil(this.currentTime);
+																		        var durationf = Math.ceil(this.duration);
+																		        var pp = $("#texto7").html();
+																				var p = pp.length;
+
+																	 			$("#duracion").text("Duracion de audio" +" : "+ durationf + " sg");
+																				$("#current").text("tiempo transcurrido" + " : " + currentTimef + " sg");
+																				$("#caracteres").text("N° de Caracteres " + n);
+																				$("#oracion").text("Caracteres en la oración " + p);
+																				$("#parrafo").text("Sistema " + i);
+																				i = i + 1;
+																				
+																				$("#audioh").off();
+																				
+
+
+																			});
+
+
+
+
+
+																		}	
+
+
+
+
+
+
+
+																	});
+
+
+
+															}	
+
+
+
+
+
+
+
+
+														});
+
+
+
+												}
+
+
+
+
+
+
+
+
+
+
+
+											});
+
+										}
+
+
+
+
+
+
+
+
+							});
+						}
+
+
+
+					});
+
+			}
+
+		});
+			
+
 	});
-	// $(".politica").mouseleave(function(){
-	// 	var audio = $("#politicasound")[0];
-	// 	audio.pause();
-	// });
-
-	// Lima
-
-	$(".lima").mouseenter(function(){
-		var audio = $("#limasound")[0];
-		audio.play();
-	});
-	$(".lima").mouseleave(function(){
-		var audio = $("#limasound")[0];
-		audio.pause();
-	});
-
-	// Peru
-
-	$(".peru").mouseenter(function(){
-		var audio = $("#perusound")[0];
-		audio.play();
-	});
-	// $(".peru").mouseleave(function(){
-	// 	var audio = $("#perusound")[0];
-	// 	audio.pause();
-	// });
-
-	// Ciencias
-
-	$(".ciencias").mouseenter(function(){
-		var audio = $("#cienciassound")[0];
-		audio.play();
-	});
-	// $(".ciencias").mouseleave(function(){
-	// 	var audio = $("#cienciassound")[0];
-	// 	audio.pause();
-	// });
-
-	// Tecnologia
-
-	$(".tecnologia").mouseenter(function(){
-		var audio = $("#tecnologiasound")[0];
-		audio.play();
-	});
-	// $(".tecnologia").mouseleave(function(){
-	// 	var audio = $("#tecnologiasound")[0];
-	// 	audio.pause();
-	// });
-
 
 });
